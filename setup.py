4b3948a658dd13eb026ef46a7aa5444d75fbea96
ZeroDesigner <luskyqi@outlook.com>
2021-05-21 00:57:15 +0800

    change name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup
-setup(name='reporter',
+setup(name='reporter_su',
       version='0.1',
       description='check your task and when it finished, you will receive a email',
       author='Su Jiaqi',
       author_email='luskyqi@outlook.com',
       python_requires=">=3.0",
-      url='https://github.com/PyDrug',
-      packages=['reporter'],
+      url='https://github.com/ZeroDesigner/reporter_su.git',
+      packages=['reporter_su'],
       license='MIT',
       keywords=['python', 'email', 'pbs']
       )
