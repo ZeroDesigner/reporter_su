98a9a7be04677a7e67e5ba247ae6c40de1270414
ZeroDesigner <luskyqi@outlook.com>
2021-05-21 01:49:04 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup
-setup(name='reporter_su',
-      version='0.1',
-      description='check your task and when it finished, you will receive a email',
-      author='Su Jiaqi',
-      author_email='luskyqi@outlook.com',
-      python_requires=">=3.0",
-      url='https://github.com/ZeroDesigner/reporter_su.git',
-      packages=['reporter_su'],
-      license='MIT',
-      keywords=['python', 'email', 'pbs']
+f = open('README.md','r')
+long_description = f.read()
+setup(name = 'reporter_su',
+      version = '0.1.1',
+      description = 'check your task and when it finished, you will receive a email',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      author = 'Su Jiaqi',
+      author_email = 'luskyqi@outlook.com',
+      python_requires = ">=3.0",
+      url = 'https://github.com/ZeroDesigner/reporter_su.git',
+      packages = ['reporter_su'],
+      license = 'MIT',
+      keywords = ['python', 'email', 'pbs']
       )
